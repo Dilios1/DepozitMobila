f883159fc76cde2211c6d4a39901708f1352e7cf
Stefan Catargiu <98192537+Dilios1@users.noreply.github.com>
2023-03-20 16:26:51 +0200

    Add files via upload

--- DepozitMobila.cs
+++ DepozitMobila.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 class DepozitMobila
 {
@@ -22,4 +25,67 @@ class DepozitMobila
     {
         MobilaDisponibila.Remove(mobila);
     }
+
+    public List<Mobila> CautaMobila(string criteriu, string valoare)
+    {
+        List<Mobila> mobilaGasita = new List<Mobila>();
+
+        switch (criteriu)
+        {
+            case "tip":
+                mobilaGasita = MobilaDisponibila.Where(m => m.TipMobila == valoare).ToList();
+                break;
+            case "pret":
+                double pretCautat = double.Parse(valoare);
+                mobilaGasita = MobilaDisponibila.Where(m => m.Pret == pretCautat).ToList();
+                break;
+            default:
+                Console.WriteLine("Criteriu invalid!");
+                break;
+        }
+
+        return mobilaGasita;
+    }
+
+    public void SalveazaDate(string numeFisier)
+    {
+        using (StreamWriter sw = new StreamWriter(numeFisier))
+        {
+            foreach (Mobila mobila in MobilaDisponibila)
+            {
+                sw.WriteLine($"{mobila.TipMobila},{mobila.Nume},{mobila.Material},{mobila.Culoare},{mobila.Cantitate},{mobila.Pret}");
+            }
+        }
+        Console.WriteLine($"Datele au fost salvate în fișierul {numeFisier}");
+    }
+
+    public static DepozitMobila IncarcareDinFisierText(string numeFisier)
+    {
+        DepozitMobila depozit = new DepozitMobila("Nume proprietar", "Adresa depozit");
+
+        using (StreamReader sr = new StreamReader(numeFisier))
+        {
+            while (!sr.EndOfStream)
+            {
+                string[] linie = sr.ReadLine().Split(';');
+
+                string tip = linie[0];
+                string nume = linie[1];
+                string material = linie[2];
+                string culoare = linie[3];
+                int pret = int.Parse(linie[4]);
+                int cantitate = int.Parse(linie[5]);
+
+                Mobila mobilier = new Mobila(tip, nume, material, culoare, pret, cantitate);
+                depozit.AdaugaMobila(mobilier);
+            }
+        }
+
+        Console.WriteLine("Datele au fost incarcate cu succes din fisierul " + numeFisier);
+        return depozit;
+    }
+
 }
+
+  
+
