adefdd28ec549a338b1125ffbc677b68cac39f8e
Stefan Catargiu <98192537+Dilios1@users.noreply.github.com>
2023-03-20 16:29:21 +0200

    Add files via upload

--- Mobila.cs
+++ Mobila.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 class Mobila
 {
-    public int Id { get; set; }
+    public string TipMobila { get; set; }
     public string Nume { get; set; }
+    public string Material { get; set; }
+    public string Culoare { get; set; }
     public int Cantitate { get; set; }
-    public float Pret { get; set; }
+    public double Pret { get; set; }
 
-    public Mobila(int id, string nume, int cantitate, float pret)
+    public static List<Mobila> listaMobila = new List<Mobila>();
+
+    public Mobila(string tipMobila, string nume, string material, string culoare, int cantitate, double pret)
     {
-        Id = id;
+        TipMobila = tipMobila;
         Nume = nume;
+        Material = material;
+        Culoare = culoare;
         Cantitate = cantitate;
         Pret = pret;
     }
@@ -24,4 +33,35 @@ class Mobila
     {
         Cantitate = cantitate;
     }
+
+    public static void CitesteDate()
+    {
+        Console.WriteLine("Introduceti datele despre mobila: ");
+
+        Console.Write("Tip mobila (scaun, masa, dulap): ");
+        string tipMobila = Console.ReadLine();
+
+        Console.Write("Nume: ");
+        string nume = Console.ReadLine();
+
+        Console.Write("Material: ");
+        string material = Console.ReadLine();
+
+        Console.Write("Culoare: ");
+        string culoare = Console.ReadLine();
+
+        Console.Write("Pret: ");
+        double pret = double.Parse(Console.ReadLine());
+
+        Console.Write("Cantitate:");
+        int cantitate = int.Parse(Console.ReadLine());    
+
+        Mobila mobila = new Mobila(tipMobila, nume, material, culoare, cantitate, pret);
+        listaMobila.Add(mobila);
+
+        Console.WriteLine("Datele au fost adaugate cu succes!");
+    }
+
+
+   
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Mobila mobila1 = new Mobila(1, "Canapea", 5, 1500);
-        Mobila mobila2 = new Mobila(2, "Masa", 10, 500);
+        Mobila mobila1 = new Mobila("Scaun", "Scaun ergonomic", "Mesh", "Negru", 5, 500);
+        Mobila mobila2 = new Mobila("Dulap", "Dulap Irim Detroit", "Lemn",  "Negru", 10, 1700);
 
-        DepozitMobila depozit = new DepozitMobila("John Doe", "Str. Principală 10");
+        DepozitMobila depozit = new DepozitMobila("Stefan Catargiu", "Str. Principală 10");
         depozit.AdaugaMobila(mobila1);
         depozit.AdaugaMobila(mobila2);
 
@@ -17,5 +21,50 @@ class Program
         {
             Console.WriteLine("- " + mobila.GetNume() + ", cantitate: " + mobila.Cantitate + ", pret: " + mobila.Pret);
         }
+
+        DepozitMobila depozit1 = new DepozitMobila("Mihai Andrei", "Str. Veronica Micle 18");
+
+        // Citirea datelor de la tastatura
+        Console.WriteLine("Introduceti detaliile pentru un mobilier:");
+        Mobila.CitesteDate();
+        depozit.AdaugaMobila(Mobila.listaMobila[0]);
+        Console.WriteLine($"Mobila adaugata: {depozit.MobilaDisponibila[0].GetNume()}");
+
+        // Salvarea datelor in fisier text
+        Console.WriteLine("Salvare date in fisier text...");
+        depozit.SalveazaDate("mobila.txt");
+
+        // Preluarea datelor dintr-un fisier text
+        Console.WriteLine("Incarcare date din fisier text...");
+        DepozitMobila depozitMobilier = DepozitMobila.IncarcareDinFisierText("depot_mobilier.txt");
+
+        // Cautarea dupa anumite criterii
+        Console.Write("Cauta dupa criteriu (tip/pret): ");
+        string criteriu = Console.ReadLine();
+
+        Console.Write("Valoare: ");
+        string valoare = Console.ReadLine();
+
+        List<Mobila> mobilaGasita = depozit.CautaMobila(criteriu, valoare);
+
+        if (mobilaGasita.Count > 0)
+        {
+            Console.WriteLine("Mobila gasita:");
+            foreach (Mobila mobila in mobilaGasita)
+            {
+                Console.WriteLine(mobila.GetNume());
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nu a fost gasita nicio mobila care sa corespunda criteriului dat.");
+        }
+
+
+
+
+
+
     }
 }
+
